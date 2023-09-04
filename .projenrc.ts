c62f893a4c66002204fa0813eae4a57944faece0
yicr <yicr@users.noreply.github.com>
2023-09-04 10:31:26 +0900

    chore: workflow node version set to 18.17.1

--- .projenrc.ts
+++ .projenrc.ts
@@ -38,7 +38,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
   minNodeVersion: '16.0.0',
-  workflowNodeVersion: '16.19.1',
+  workflowNodeVersion: '18.17.1',
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
