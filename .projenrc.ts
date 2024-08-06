e6b13a55c4bd0dd78419fe78642c71702dbd1eaf
yicr <yicr@users.noreply.github.com>
2024-08-06 17:18:43 +0900

    chore(deps): rename package shapshots serialisers

--- .projenrc.ts
+++ .projenrc.ts
@@ -22,11 +22,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
     '@slack/web-api',
     '@types/aws-lambda',
     '@yicr/aws-lambda-errors',
-    '@gammarer/jest-serializer-aws-cdk-asset-filename-replacer',
+    '@gammarers/jest-aws-cdk-asset-filename-renamer@~0.5.0',
   ],
   jestOptions: {
     jestConfig: {
-      snapshotSerializers: ['<rootDir>/node_modules/@gammarer/jest-serializer-aws-cdk-asset-filename-replacer'],
+      snapshotSerializers: ['<rootDir>/node_modules/@gammarers/jest-aws-cdk-asset-filename-renamer'],
     },
     extraCliOptions: ['--silent'],
   },
